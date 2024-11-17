4140ba5b9da4bae5381188c1ad61f44dfe2f01db
CJ Caraang <156409258+CJCJsC@users.noreply.github.com>
2024-11-16 15:41:20 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# Adding a user node
 
+# Adding a post node with attributes
+
+# Creating connections
+# Authorship
+
+# Follows
+
+# Views
+
+# Define colors for different connection types
+
+# Iterate through nodes and calculate importance for posts
+
+# Assign node sizes based on importance score
