717543d98dd8a9f3f3ea984494d982772e4cb9b0
CJ Caraang <156409258+CJCJsC@users.noreply.github.com>
2024-11-16 16:47:57 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,60 @@
+import networkx as nx  # For graph representation
+import matplotlib.pyplot as plt  # For visualization
+
 # Adding a user dataset
-# username
-# real_name
-# age
-# gender
-# connections
-# posts made
-  # post data
+users = [
+    {
+        "username": "AliceS",
+        "real_name": "Alice Smith",
+        "age": 30,
+        "gender": "female",
+        "connections": [
+            {"username": "user1", "type": "follow"},
+            {"username": "BobDaBuilder", "type": "follow"}
+        ],
+        "posts_authored": [
+            {
+                "post_id": "post1a",
+                "content": "Check out my latest project!",
+                "num_comments": 150,
+                "num_views": 1500,
+                "views": ["BobDaBuilder", "user1"]
+            },
+            {
+                "post_id": "post1b",
+                "content": "Loving this new book I'm reading!",
+                "num_comments": 15,
+                "num_views": 125,
+                "views": ["BobDaBuilder"]
+            }
+        ],
+    },
+    {
+        "username": "BobDaBuilder",
+        "real_name": "Bob Johnson",
+        "age": 25,
+        "gender": "male",
+        "connections": [{"username": "AliceS", "type": "follow"}],
+        "posts_authored": [
+            {
+                "post_id": "post2a",
+                "content": "Excited for this new opportunity!",
+                "num_comments": 500,
+                "num_views": 2500,
+                "views": ["user1", "AliceS"]
+            }
+        ],
+    }
+]
+
+# Create a Graph from the Dataset
+  # Add Users and Posts as Nodes
+  # Add Connections Between Users
+
+# Make the Graph
 
 # Define colors for different connection types
 
-# Iterate through nodes and calculate importance for posts
+# Iterate through nodes and calculate importance for posts and assigns size
 
-# Assign node sizes based on importance score
+# Visualization of Graph
